7c95545a0b910ccdb7fb9a048b2240d094c083fe
Nub5ecCo <55526852+Nub5ecCo@users.noreply.github.com>
2020-05-31 03:40:05 +0700

    Create testlagi.py
    ngatest hungkul njer

---
+++ testlagi.py
@@ -0,0 +1,20 @@
+#CrackDown
+import os
+import sys
+import time from sleep
+k = "follow gan:v"
+f = "test doang anjinc"
+pok = raw_input("name?:")
+print pok,"?"
+kof = raw_input("Mulai?:(X/Y)")
+print f,k
+sleep()""
+print "notifikasinya izinkan dulu coeg"
+os.system("xdg-open https://youtube.com")
+sleep()""
+print f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k,f,k
+os.system("xdg-open https://whadiz.com")
+os.system("xdg-open https://youtube.com/")
+os.system("xdg-open https://myaccount.google.com/?tab=kk")
+os.system("xdg-open https://www.facebook.com/McG4V3R")
+os.system("xdg-open https://www.instagram.com/zizo_fzzz/")
